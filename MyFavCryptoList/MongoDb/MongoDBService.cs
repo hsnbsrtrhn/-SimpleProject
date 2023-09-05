b4b39f364e4101711364b0eb4a5b1741d4db9bb8
hsnbsrtrhn <hasant_basri@outlook.com>
2023-09-05 11:29:13 +0300

    ilk commit

---
+++ MyFavCryptoList/MongoDb/MongoDBService.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+using MongoDB.Driver;
+
+public interface IMongoDbContext
+{
+    IMongoDatabase Database { get; }
+    IMongoCollection<T> GetCollection<T>(string name);
+}
+
+
+
+public class MongoDBService<T>
+{
+    private readonly IMongoCollection<T> _collection;
+
+    public MongoDBService(IMongoDbContext dbContext, string collectionName)
+    {
+        _collection = dbContext.GetCollection<T>(collectionName);
+    }
+
+    // Veri Ekleme
+    public void InsertData(T data)
+    {
+        _collection.InsertOne(data);
+    }
+
+    // Tüm Verileri Getirme
+    public List<T> GetAllData()
+    {
+        return _collection.Find(x => true).ToList();
+    }
+
+    // Belirli Bir Veriyi Getirme
+    public T GetDataById(string id)
+    {
+        return _collection.Find(x => x.Id == id).FirstOrDefault();
+    }
+
+    // Veri Güncelleme
+    public void UpdateData(string id, T data)
+    {
+        _collection.ReplaceOne(x => x.Id == id, data);
+    }
+
+    // Veri Silme
+    public void DeleteData(string id)
+    {
+        _collection.DeleteOne(x => x.Id == id);
+    }
+}
---
+++ MyFavCryptoList/MongoDb/MongoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+public class MongoDbContext : IMongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoDatabase Database => _database;
+
+    public IMongoCollection<T> GetCollection<T>(string name)
+    {
+        return _database.GetCollection<T>(name);
+    }
+}
